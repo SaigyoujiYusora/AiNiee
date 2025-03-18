e2cb1d64a5d9519de419d2463b81cab430b0b35b
KawaiiYusora <misaka09983@gmail.com>
2025-03-19 04:18:04 +0800

    ✨ refactor(TranslatorRequester): 调整 tokens_limit 乘数从 1.3 改为 2

--- ModuleFolders/Translator/TranslatorRequester.py
+++ ModuleFolders/Translator/TranslatorRequester.py
@@ -99,7 +99,7 @@ class TranslatorRequester(Base):
                 frequency_penalty=frequency_penalty,
                 timeout=request_timeout,
                 max_tokens=max(512,
-                               int(self.config.tokens_limit * 1.3)) if self.config.tokens_limit_switch == True else 512,
+                               int(self.config.tokens_limit * 2)) if self.config.tokens_limit_switch == True else 512,
                 extra_query={
                     "do_sample": True,
                     "num_beams": 1,
@@ -481,7 +481,7 @@ class TranslatorRequester(Base):
                     timeout=request_timeout,
                     stream=False,
                     max_tokens=max(512,
-                                   int(self.config.tokens_limit * 1.3)) if self.config.tokens_limit_switch == True else 512,
+                                   int(self.config.tokens_limit * 2)) if self.config.tokens_limit_switch == True else 512,
                 )
 
             else:
